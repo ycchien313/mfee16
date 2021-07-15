976fea2e7c6c0aa781bdc014c3bd129833ed729c
YiChieh <chien313jay@gmail.com>
2021-07-15 12:23:58 +0800

    init. frontend

--- forestage/frontend/src/App.js
+++ forestage/frontend/src/App.js
@@ -1,13 +1,41 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
-// import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Comment from './pages/Comment'
+import Delivery from './pages/Delivery'
+import Dish from './pages/Dish'
+import Game from './pages/Game'
+import Home from './pages/Home'
+import Member from './pages/Member'
+import Reservation from './pages/Reservation'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/auth/">
+          <Auth />
+        </Route>
+        <Route path="/comment/">
+          <Comment />
+        </Route>
+        <Route path="/delivery/">
+          <Delivery />
+        </Route>
+        <Route path="/dish/">
+          <Dish />
+        </Route>
+        <Route path="/game/">
+          <Game />
+        </Route>
+        <Route path="/member/">
+          <Member />
+        </Route>
+        <Route path="/reservation/">
+          <Reservation />
+        </Route>
         <Route exact path="/">
-          {/* <Home /> */}
+          <Home />
         </Route>
       </Switch>
     </Router>

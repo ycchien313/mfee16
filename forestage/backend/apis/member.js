ae34c788bc9dd029366f61665da37578e23703c6
YiChieh <chien313jay@gmail.com>
2021-07-15 21:22:19 +0800

    fix. member.js

--- forestage/backend/apis/member.js
+++ forestage/backend/apis/member.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../utils/db');
+
+router.get('/', () => {
+    console.log(1);
+});
 
 module.exports = router;

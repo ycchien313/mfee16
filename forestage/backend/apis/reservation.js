02b4fbf06d973ddd2760589861024ab241e6e3e6
Sandy Lee <75609720+huiyu580@users.noreply.github.com>
2021-07-16 23:06:04 +0800

    add reservation

--- forestage/backend/apis/reservation.js
+++ forestage/backend/apis/reservation.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../utils/db')
 
-router.get('/', async (req, res)=>{
-
+router.get('/seat', async (req, res)=>{
     let getSeatSql = 'SELECT * FROM seat'
     let result = await db.connection.queryAsync(getSeatSql)
-    res.send(result)
+    res.json(result)
 })
+
 
 module.exports = router;

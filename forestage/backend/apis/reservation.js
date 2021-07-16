e35928b23774e8f3a33fa078f587925e8d2ba7e4
huiyu580 <huiyu.lee580@gmail.com>
2021-07-16 13:46:17 +0800

    get seat Data

--- forestage/backend/apis/reservation.js
+++ forestage/backend/apis/reservation.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../utils/db')
+
+router.get('/', async (req, res)=>{
+
+    let getSeatSql = 'SELECT * FROM seat'
+    let result = await db.connection.queryAsync(getSeatSql)
+    res.send(result)
+})
 
 module.exports = router;

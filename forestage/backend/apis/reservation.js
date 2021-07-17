68ac51efeda54a06b0e54cbc4e3472324075b318
Sandy Lee <75609720+huiyu580@users.noreply.github.com>
2021-07-17 22:17:10 +0800

    update remainingSeats

--- forestage/backend/apis/reservation.js
+++ forestage/backend/apis/reservation.js
@@ -16,11 +16,16 @@ router.get('/singer-calendar', async(req, res)=>{
 
 router.get('/:date', async(req, res)=>{
     // let getReservationSeatTotal = 'SELECT seat_id, attendance FROM reservation WHERE date=? GROUP BY seat_id'
-    let getReservationSeatTotal = 'SELECT s.name, SUM(r.attendance), s.seat_id FROM reservation r, seat s WHERE date=? AND s.seat_id = r.seat_id GROUP BY seat_id'
-    let reservationSeatTotal = await db.connection.queryAsync(getReservationSeatTotal,[req.params.date])
+    let getRemainingSeatsSql = 'SELECT s.name, s.seat_number-SUM(r.attendance) AS remainingSeats, s.seat_id FROM reservation r, seat s WHERE date=? AND s.seat_id = r.seat_id GROUP BY seat_id'
+
+    let RemainingSeats = await db.connection.queryAsync(getRemainingSeatsSql,[req.params.date])
     
-    res.send(reservationSeatTotal)
+    res.send(RemainingSeats)
     // console.log(req.params.date)
+})
+
+router.get('/seat', async(req, res)=> {
+    let getSeatInfoSql = ''
 })
 
 module.exports = router;

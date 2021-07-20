265ba546c43dd329118ad6d1ee5cca97dc4b1a33
YiChieh <chien313jay@gmail.com>
2021-07-21 01:50:41 +0800

    feat. CORS

--- forestage/backend/app.js
+++ forestage/backend/app.js
@@ -10,7 +10,34 @@ const reservationApi = require('./apis/reservation');
 const singerApi = require('./apis/singer');
 const db = require('./utils/db');
 const connection = db.connection;
+const cors = require('cors');
 const port = 3001;
+
+// 加入 headers
+// app.use(function (req, res, next) {
+//     // 允許連接的網頁
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     // 允許的方法
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//     );
+
+//     // Request headers you wish to allow
+//     // res.setHeader(
+//     //     'Access-Control-Allow-Headers',
+//     //     'X-Requested-With,content-type'
+//     // );
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+app.use(cors());
 
 app.use('/auth', authApi);
 app.use('/comment', commentApi);

ed23c4b3562d69931971ddcb7ece1ced22959833
YiChieh <chien313jay@gmail.com>
2021-07-22 02:45:52 +0800

    feat. move axios dbRequest to here

--- forestage/frontend/src/components/Member/Profile/Main/index.js
+++ forestage/frontend/src/components/Member/Profile/Main/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Aside from '../../Common/Main/Aside'
 import Breadcrumb from '../../Common/Main/Breadcrumb'
 import PersonalInfo from './PersonalInfo'
@@ -6,6 +7,17 @@ import SigninInfo from './SigninInfo'
 
 function Main(props) {
   const { pagename } = props
+
+  const memberId = 1
+
+  // server 端會員資料的請求
+  const dbRequest = axios.create({
+    baseURL: 'http://127.0.0.1:3001/member/',
+    headers: new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'appliaction/json',
+    }),
+  })
 
   return (
     <>
@@ -24,13 +36,13 @@ function Main(props) {
               <div className="content-container">
                 <div className="info-container">
                   {/* <!-- 左側：會員照片、個人資料 --> */}
-                  <PersonalInfo />
+                  <PersonalInfo dbRequest={dbRequest} memberId={memberId} />
 
                   {/* <!-- 中間分隔線 --> */}
                   <div className="vertical-line"></div>
 
                   {/* <!-- 右側：登入資料 --> */}
-                  <SigninInfo />
+                  <SigninInfo dbRequest={dbRequest} memberId={memberId} />
                 </div>
               </div>
             </div>

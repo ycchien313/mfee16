cd97207a12afbace4658e4dc54c8b8185d669f17
YiChieh <chien313jay@gmail.com>
2021-07-23 14:21:54 +0800

    add. page of member's reservation

--- forestage/frontend/src/pages/Member/index.js
+++ forestage/frontend/src/pages/Member/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route, withRouter } from 'react-router-dom'
 import Profile from '../../components/Member/Profile/'
+import Reservation from '../../components/Member/Reservation/'
 
 function Member(props) {
   const url = props.match.url
 
   return (
     <Switch>
+      <Route path={`${url}/reservation`}>
+        <Reservation />
+      </Route>
       <Route path={`${url}/profile`}>
         <Profile pagename="會員資料" />
       </Route>

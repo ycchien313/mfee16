502d0be9572cfe66b8ae3f198b32bbc8e9176550
YiChieh <chien313jay@gmail.com>
2021-07-25 22:59:31 +0800

    feat. modal props, div of popup layer

--- forestage/frontend/src/components/Auth/index.js
+++ forestage/frontend/src/components/Auth/index.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react'
+import '../../styles/auth/auth.scss'
 import Signin from './Signin/'
 import Signup from './Signup/'
 
-function Auth() {
-  // true: Signin
-  // false: Signup
+function Auth(props) {
+  const { showAuthModal, setShowAuthModal } = props
+  // true: Signin，false: Signup
   const [signinScreen, setSigninScreen] = useState(true)
 
   return (
     <>
-      {signinScreen ? (
-        <Signin signinScreen={signinScreen} setSigninScreen={setSigninScreen} />
-      ) : (
-        <Signup signinScreen={signinScreen} setSigninScreen={setSigninScreen} />
-      )}
+      <div
+        className="auth"
+        onClick={(e) => {
+          e.target.className === 'auth' && setShowAuthModal(!showAuthModal)
+          console.log(e)
+        }}
+      >
+        {signinScreen ? (
+          <Signin
+            signinScreen={signinScreen}
+            setSigninScreen={setSigninScreen}
+          />
+        ) : (
+          <Signup
+            signinScreen={signinScreen}
+            setSigninScreen={setSigninScreen}
+          />
+        )}
+      </div>
     </>
   )
 }

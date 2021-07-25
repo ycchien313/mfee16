23127d7758a8206d8175a32527b74c3eeed72d28
YiChieh <chien313jay@gmail.com>
2021-07-25 23:02:53 +0800

    feat. login, send data to backend, set token, loading and transition, fix. signin's form

--- forestage/frontend/src/components/Auth/AuthForm.js
+++ forestage/frontend/src/components/Auth/AuthForm.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 import Swal from 'sweetalert2'
+import { setAuthToken } from './utils'
 
 function AuthForm(props) {
-  const { signinScreen } = props
+  const { match, location, history, signinScreen } = props
   const [errorMsg, setErrorMsg] = useState('')
   const [addr, setAddr] = useState({ city: '桃園市', street: '' })
   const [cityOptions, setCityOptions] = useState(['桃園市', '台北市', '新北市'])
-  const [signupField, setSignupField] = useState({
+  const [signupFields, setSignupFields] = useState({
     email: 'angel@gmail.com',
     password: '111',
     confirmPassword: '111',
     mobile: '0912345678',
     address: '桃園市',
+  })
+  const [signinFields, setSigninFields] = useState({
+    account: 'angel@gmail.com',
+    password: '123',
   })
 
   const serverRequest = axios.create({
@@ -28,7 +34,7 @@ function AuthForm(props) {
     const passwordInput = e.target['password']
     const confirmPasswordInput = e.target['confirmPassword']
 
-    if (signupField.password !== signupField.confirmPassword) {
+    if (signupFields.password !== signupFields.confirmPassword) {
       // 新增驗證錯誤背景
       passwordInput.classList.add('bg-lightgray')
       confirmPasswordInput.classList.add('bg-lightgray')
@@ -46,11 +52,30 @@ function AuthForm(props) {
 
   // 處理 input onChange
   const handleInputChange = (e) => {
-    let newSignupField = { ...signupField, [e.target.name]: e.target.value }
-    setSignupField(newSignupField)
+    switch (signinScreen) {
+      case true:
+        let newSigninFields = {
+          ...signinFields,
+          [e.target.name]: e.target.value,
+        }
+        setSigninFields(newSigninFields)
+        break
+
+      case false:
+        let newSignupFields = {
+          ...signupFields,
+          [e.target.name]: e.target.value,
+        }
+        setSignupFields(newSignupFields)
+        break
+
+      default:
+        break
+    }
 
     e.target.classList.remove('bg-lightgray')
 
+    // 清空錯誤訊息
     setErrorMsg('')
   }
 
@@ -59,12 +84,12 @@ function AuthForm(props) {
     let newAddr = { ...addr, [e.target.name]: e.target.value }
     setAddr(newAddr)
 
-    // 當地址有變動，馬上處理 signupField 狀態的 address 欄位
+    // 當地址有變動，馬上處理 signupFields 狀態的 address 欄位
     let newSignupField = {
-      ...signupField,
+      ...signupFields,
       address: `${newAddr.city}${newAddr.street}`,
     }
-    setSignupField(newSignupField)
+    setSignupFields(newSignupField)
   }
 
   // 處理 驗證訊息
@@ -72,7 +97,7 @@ function AuthForm(props) {
     e.preventDefault()
     e.target.classList.add('bg-lightgray')
 
-    // 從第一個錯誤開始顯示
+    // 從第一個錯誤開始顯示，須配合 handleInputChange()
     errorMsg === '' && setErrorMsg(e.target.title)
   }
 
@@ -81,40 +106,65 @@ function AuthForm(props) {
     e.preventDefault()
 
     switch (signinScreen) {
+      // 登入
       case true:
-        break
-
-      case false:
-        if (!checkPassword(e)) return
-
-        console.log('準備傳入後端，資料為: ', signupField)
+        console.log('準備傳入後端，資料為: ', signinFields)
 
         try {
-          const response = await serverRequest.post('/signup', signupField)
+          const response = await serverRequest.post('/signin', signinFields)
           const data = response.data
-          const status = data.status
+          const result = data.result
           const msg = data.msg
+          const token = data.token
 
-          // 註冊失敗
-          if (msg === '此 email 已有人註冊') {
-            e.target['email'].classList.add('bg-lightgray')
+          // 登入失敗
+          if (result === '失敗') {
             setErrorMsg(msg)
             return
-          } else if (msg === '註冊失敗') {
+          }
+
+          // 登入成功
+          console.log('登入成功，token: ', token)
+          // 設定 token for localStorage
+          setAuthToken(token)
+          // To Do 跳出彈出式視窗 (跳轉到當時頁面)
+
+          // 載入指示器及轉場
+          successfulTransition('登入成功')
+        } catch (error) {}
+        break
+
+      // 註冊
+      case false:
+        // 密碼與確認密碼不一致
+        if (!checkPassword(e)) return
+
+        console.log('準備傳入後端，資料為: ', signupFields)
+
+        try {
+          const response = await serverRequest.post('/signup', signupFields)
+          const data = response.data
+          const result = data.result
+          const msg = data.msg
+          const token = data.token
+
+          // 註冊失敗
+          if (result === '失敗') {
+            msg === '此 email 已有人註冊' &&
+              e.target['email'].classList.add('bg-lightgray')
+
             setErrorMsg(msg)
             return
           }
 
           // 註冊成功
-          setTimeout(() => {
-            Swal.fire({
-              icon: 'success',
-              title: '註冊成功',
-              showConfirmButton: false,
-              timer: 1000,
-            })
-            // To Do 跳轉至當時頁面(訂餐頁 or 首頁 ...)
-          }, 1000)
+          console.log('註冊成功，token', token)
+          // 設定 token for localStorage
+          setAuthToken(token)
+          // To Do 跳出彈出式視窗 (跳轉到當時頁面)
+
+          // 載入指示器及轉場
+          successfulTransition('註冊成功')
         } catch (error) {
           // 內部錯誤
           console.error('error:', error)
@@ -126,15 +176,57 @@ function AuthForm(props) {
     }
   }
 
+  // 載入指示器及轉場
+  const successfulTransition = (title) => {
+    Swal.fire({
+      html:
+        '<div>' +
+        '<img src="http://127.0.0.1:3000//images/auth/spinner.svg" alt=""></img>' +
+        '</div>',
+      showConfirmButton: false,
+      timer: 750,
+    })
+    setTimeout(() => {
+      Swal.fire({
+        icon: 'success',
+        title: title,
+        showConfirmButton: false,
+        timer: 1000,
+      })
+    }, 750)
+  }
+
   const signinDom = (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onInvalid={handleInvalid}>
         <div className="form-container">
-          <label htmlFor="email">帳號 / 電子郵件</label>
-          <input name="eamil" type="text" />
+          <label htmlFor="email">帳號 / E-mail</label>
+          <input
+            name="account"
+            type="text"
+            required
+            title="請輸入帳號或 E-mail"
+            value={signinFields.account}
+            onChange={(e) => {
+              handleInputChange(e)
+            }}
+          />
           <label htmlFor="password">密碼</label>
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            placeholder="共 3 ~ 8 個字元"
+            minLength="3"
+            maxLength="8"
+            required
+            title="密碼長度為 3 ~ 8 個字元"
+            value={signinFields.password}
+            onChange={(e) => {
+              handleInputChange(e)
+            }}
+          />
           <div className="btn-row">
+            <span className="error-msg">{errorMsg}</span>
             <button className="orange-guide-button">
               <span>送出</span>
               <i className="fas fa-arrow-circle-right"></i>
@@ -150,12 +242,12 @@ function AuthForm(props) {
       <form onSubmit={handleSubmit} onInvalid={handleInvalid}>
         <div className="form-container">
           <div className="form-row">
-            <label htmlFor="email">E-Mail(此為以後登入帳號)</label>
+            <label htmlFor="email">E-mail (此為以後登入帳號)</label>
             <input
               name="email"
               type="email"
-              value={signupField.email}
-              title="請輸入正確的email"
+              value={signupFields.email}
+              title="請輸入正確的 E-mail"
               required
               onChange={(e) => {
                 handleInputChange(e)
@@ -168,11 +260,12 @@ function AuthForm(props) {
               <input
                 name="password"
                 type="password"
+                placeholder="共 3 ~ 8 個字元"
                 minLength="3"
                 maxLength="8"
                 required
-                title="密碼字數為 3-8"
-                value={signupField.password}
+                title="密碼字數為 3~8 個字元"
+                value={signupFields.password}
                 onChange={(e) => {
                   handleInputChange(e)
                 }}
@@ -183,11 +276,12 @@ function AuthForm(props) {
               <input
                 name="confirmPassword"
                 type="password"
+                placeholder="共 3 ~ 8 個字元"
                 minLength="3"
                 maxLength="8"
                 required
-                title="確認密碼字數為 3-8"
-                value={signupField.confirmPassword}
+                title="確認密碼字數為 3 ~ 8 個字元"
+                value={signupFields.confirmPassword}
                 onChange={(e) => {
                   handleInputChange(e)
                 }}
@@ -199,7 +293,7 @@ function AuthForm(props) {
             <input
               name="mobile"
               type="tel"
-              value={signupField.mobile}
+              value={signupFields.mobile}
               placeholder="ex. 0911222333"
               pattern="([0-9]{4}-[0-9]{3}-[0-9]{3})|([0-9]{4}[0-9]{3}[0-9]{3})"
               required
@@ -255,4 +349,4 @@ function AuthForm(props) {
   return <>{signinScreen ? signinDom : signupDom}</>
 }
 
-export default AuthForm
+export default withRouter(AuthForm)

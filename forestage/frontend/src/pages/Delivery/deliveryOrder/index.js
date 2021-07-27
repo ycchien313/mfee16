27eb569ac4e99584d60a68a4ee8f5141b78b33b0
luofungyi <teddyluo8233@gmail.com>
2021-07-27 16:10:24 +0800

    add

--- forestage/frontend/src/pages/Delivery/deliveryOrder/index.js
+++ forestage/frontend/src/pages/Delivery/deliveryOrder/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../../../styles/delivery/deliveryOrder/takeout_check.scss'
 import DeliveryOrder from './components/DeliveryOrder'
+import Header from '../../../components/Header'
 
 function index() {
   return (
     <>
+      <Header />
       <DeliveryOrder />
     </>
   )

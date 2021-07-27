b0c1d39837feb578cace2ab593e96727131a3979
YiChieh <chien313jay@gmail.com>
2021-07-28 03:46:18 +0800

    fix. passport.js function

--- forestage/backend/app.js
+++ forestage/backend/app.js
@@ -11,29 +11,16 @@ const singerApi = require('./apis/singer');
 const db = require('./utils/db');
 const connection = db.connection;
 const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const facebookStrategy = require('passport-facebook').Strategy;
 const port = 3001;
 
 require('dotenv').config();
 
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-app.use(
-    session({
-        secret: 'keyboard cat',
-        resave: true,
-        saveUninitialized: true,
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/auth', authApi);
 app.use('/comment', commentApi);
@@ -44,67 +31,6 @@ app.use('/member', memberApi);
 app.use('/reservation', reservationApi);
 app.use('/singer', singerApi);
 
-passport.use(
-    new facebookStrategy(
-        {
-            // pull in our app id and secret from our auth.js file
-            clientID: process.env.FACEBOOK_ID,
-            clientSecret: process.env.FACEBOOK_SECRET,
-            // callbackURL: 'https://localhost:8443/facebook/callback',
-            callbackURL: 'http://localhost:3001/facebook/callback',
-            profileFields: [
-                'id',
-                'displayName',
-                'name',
-                'gender',
-                'picture.type(large)',
-                'email',
-            ],
-        }, // facebook will send back the token and profile
-
-        function (accesstoken, refreshToken, profile, done) {
-            // console.log(profile);
-            // return done(null, profile);
-
-            User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-                return cb(err, user);
-            });
-        }
-    )
-);
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-
-// used to deserialize the user
-passport.deserializeUser(function (id, done) {
-    return done(null, user);
-});
-
-app.get('/profile', (req, res) => {
-    console.log(req.url);
-    res.send('you are authenticated');
-});
-
-app.get(
-    '/facebook',
-    passport.authenticate('facebook', { scope: 'email, name' }),
-    (req, res) => {
-        console.log(req.url);
-    }
-);
-
-app.get(
-    '/facebook/callback',
-    passport.authenticate('facebook', {
-        successRedirect: '/profile',
-        failureRedirect: '/',
-    }),
-    (req, res) => {
-        console.log(req.url);
-    }
-);
 app.get('/', (req, res) => {
     console.log('URL:', req.url);
     res.status(200).json({ url: req.url });

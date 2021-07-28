6a00232a58e4647d8b237568540ede167c94e26e
YiChieh <chien313jay@gmail.com>
2021-07-29 05:58:15 +0800

    fix. router with context member and delay

--- forestage/frontend/src/pages/Member/index.js
+++ forestage/frontend/src/pages/Member/index.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Switch, Route, withRouter } from 'react-router-dom'
+import ReactDelay from 'react-delay'
 import Profile from '../../components/Member/Profile/'
 import Reservation from '../../components/Member/Reservation/'
+import AuthContext from '../../components/Auth/AuthContext'
+import Auth from '../../components/Auth'
 
 function Member(props) {
+  const { member } = useContext(AuthContext)
+  const [showAuthModal, setShowAuthModal] = useState(false)
   const url = props.match.url
 
   return (
-    <Switch>
-      <Route path={`${url}/reservation`}>
-        <Reservation />
-      </Route>
-      <Route path={`${url}/profile`}>
-        <Profile pagename="會員資料" />
-      </Route>
-      <Route exact path={`${url}`}>
-        <Profile pagename="會員資料" />
-      </Route>
-    </Switch>
+    <>
+      <ReactDelay wait={500}>
+        {member !== null ? (
+          <Switch>
+            <Route path={`${url}/reservation`}>
+              <Reservation pagename="我的訂位" />
+            </Route>
+            <Route path={`${url}/profile`}>
+              <Profile pagename="會員資料" />
+            </Route>
+            <Route exact path={`${url}`}>
+              <Profile pagename="會員資料" />
+            </Route>
+          </Switch>
+        ) : (
+          <Auth
+            showAuthModal={showAuthModal}
+            setShowAuthModal={setShowAuthModal}
+          />
+        )}
+      </ReactDelay>
+    </>
   )
 }
 

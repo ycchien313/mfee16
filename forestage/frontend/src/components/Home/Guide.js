346da8b63465444ab2d123b57963df0daf786c19
Vincent Jan <yiyanzhan0823@gmail.com>
2021-08-06 00:10:31 +0800

    live crawler completed

--- forestage/frontend/src/components/Home/Guide.js
+++ forestage/frontend/src/components/Home/Guide.js
@@ -1,15 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { YoutubeDataAPI } from 'youtube-v3-api'
 function Guide(props) {
   let { guideButton, leftVision, guideIndex, guideTitle, guideContent } = props
+  const [liveID, setLiveID] = useState('')
+  // 取得直播網址
+  const API_KEY = 'AIzaSyA7Npb4e2rpovwMq30UbRFWop_cjiIDTiM'
 
+  const api = new YoutubeDataAPI(API_KEY)
+
+  api.searchAll(`Elfin 詹宜諺`, 1).then(function (result) {
+    setLiveID(result.items[0].id.videoId)
+  })
+  //
   let leftContent = [
-    <video
-      src={'http://localhost:3000' + leftVision}
-      autoPlay
-      loop
-      muted
-    ></video>,
+    <iframe
+      width="424"
+      height="240"
+      src={`https://www.youtube.com/embed/${liveID}`}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>,
+
     <figure>
       <img src={'http://localhost:3000' + leftVision} alt="" />,
     </figure>,
